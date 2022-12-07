11ef53b136961866e34ef200cae640d2c243892a
Ery Nascimento <ery10rx@outlook.com>
2022-12-06 22:49:37 -0300

    first commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { ButtonContainer } from "./styles";
+
+function Button({onClick, text}) {
+  return (
+    <ButtonContainer onClick={onClick}>
+      {text}
+    </ButtonContainer>
+  );
+}
+
+export default Button;
